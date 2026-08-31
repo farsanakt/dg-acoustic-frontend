4de2ddd6d6103d244b6a882b9ebf604044ca3198
Farsana kt <ffarsanakt@gmail.com>
2026-08-31 18:11:11 +0530

    updated backend api

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      "/api": { target: "http://localhost:4000", changeOrigin: true },
+      "/api": { target: "https://dg-acoustic-backend.onrender.com", changeOrigin: true },
     },
   },
 });
